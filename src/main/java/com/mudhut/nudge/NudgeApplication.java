a9e10911ce43eea2ce22c86936ca050a6965d7f2
Phillip Seryazi <seryaziphillip@gmail.com>
2024-07-16 22:11:41 +0300

    first commit

---
+++ src/main/java/com/mudhut/nudge/NudgeApplication.java
@@ -0,0 +1,13 @@
+package com.mudhut.nudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NudgeApplication.class, args);
+	}
+
+}
